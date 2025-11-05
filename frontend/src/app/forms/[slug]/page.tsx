dbd407d018a2da9db362032de6f8793a9a26d528
Samir Rodas <serodas9710@gmail.com>
2025-11-05 18:16:27 -0500

    Refactor de id a slug en frontend

--- frontend/src/app/forms/[slug]/page.tsx
+++ frontend/src/app/forms/[slug]/page.tsx
@@ -14,11 +14,11 @@ interface FormData {
     fields: FormField[];
 }
 
-export default async function FormPage({ params }: { params: Promise<{ id: string }> }) {
+export default async function FormPage({ params }: { params: Promise<{ slug: string }> }) {
     const base = process.env.NEXT_PUBLIC_API_BASE;
 
-      const { id } = await params;
-    const res = await fetch(`${base}/forms/${id}/`, { cache: "no-store" });
+    const { slug } = await params;
+    const res = await fetch(`${base}/forms/${slug}/`, { cache: "no-store" });
     console.log(res);
     if (!res.ok) {
         throw new Error("No se pudo obtener el formulario");
