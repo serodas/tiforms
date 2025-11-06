2104b6f11d1ea632777cc79823a83af4cd217d21
Samir Rodas <serodas9710@gmail.com>
2025-11-06 08:21:55 -0500

    Elimina corsheader no es necesario

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -58,13 +58,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "forms",
-    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
